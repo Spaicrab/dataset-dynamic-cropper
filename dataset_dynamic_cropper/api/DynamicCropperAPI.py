a8b29bbf7616a6e046cfca20488895dfa10e70c4
Spaicrab <giuliodf2005@gmail.com>
2023-06-17 15:06:28 +0200

    Renamed database to dataset

---
+++ dataset_dynamic_cropper/api/DynamicCropperAPI.py
@@ -0,0 +1,7 @@
+from .main import main
+
+class DynamicCropperAPI:
+    def dynamic_crop(self, input_directory, image_extension, crop_size, recursive):
+        args = [input_directory, '-e', str(image_extension), '-s', str(crop_size)]
+        if recursive: args.append('-r')
+        main(args)
